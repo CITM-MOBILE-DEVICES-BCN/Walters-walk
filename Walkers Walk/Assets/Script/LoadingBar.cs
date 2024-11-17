458073a681653db6d3d4b5b07a9192942eff7677
D1dii <didacgp123@gmail.com>
2024-11-17 12:53:36 +0100

    LoadingBar implemented
    it doesnt load any scene yet

---
+++ Walkers Walk/Assets/Script/LoadingBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingBar : MonoBehaviour
+{
+    public Slider loadingBar;
+    private float loadingValue = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int randomNum = Random.Range(0, 100);
+
+        if(randomNum < 20 && loadingValue < 0.8f)
+        {
+            FakeLoading();
+        }
+        else if (loadingValue >= 0.8f)
+        {
+            StartCoroutine(completeLoad());
+        }
+    }
+
+    public void FakeLoading()
+    {
+        loadingValue += 0.01f;
+        loadingBar.value = loadingValue;
+    }
+
+    IEnumerator completeLoad()
+    {
+        yield return new WaitForSeconds(1);
+        loadingValue = 1;
+        loadingBar.value = loadingValue;
+    }
+}
