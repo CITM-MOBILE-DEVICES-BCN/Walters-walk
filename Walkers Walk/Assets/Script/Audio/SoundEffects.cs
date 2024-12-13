4b081845c208b8ac9eedbf4f23ea5f2d0209a46f
Biel <biel.linan42@gmail.com>
2024-12-13 16:36:14 +0100

    AudioSystem almost finnished
    added car horn, truck horn, bacgkround ambient and music

--- Walkers Walk/Assets/Script/Audio/SoundEffects.cs
+++ Walkers Walk/Assets/Script/Audio/SoundEffects.cs
@@ -4,14 +4,70 @@ using UnityEngine;
 
 public class SoundEffects : MonoBehaviour
 {
-    public AudioSource audiosystem;
-    public AudioClip sfx1;
+    public AudioSource audiosystem; 
+    public AudioClip sfx1; 
+    public AudioClip sfx2; 
+    public AudioClip sfx3; 
+    public Transform player;
+    public float activationDistance = 10f; 
 
-    public void Bike()
+    private void Update()
     {
-        audiosystem.clip = sfx1;
-        audiosystem.Play();
+        bool soundPlayed = false;
 
+        GameObject[] bikes = GameObject.FindGameObjectsWithTag("Bike");
+        foreach (GameObject bike in bikes)
+        {
+            if (Vector3.Distance(bike.transform.position, player.position) <= activationDistance)
+            {
+                PlaySound(sfx1);
+                soundPlayed = true;
+                break; 
+            }
+        }
+
+        if (!soundPlayed)
+        {
+            GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
+            foreach (GameObject car in cars)
+            {
+                if (Vector3.Distance(car.transform.position, player.position) <= activationDistance)
+                {
+                    PlaySound(sfx2);
+                    soundPlayed = true;
+                    break; 
+                }
+            }
+        }
+        if (!soundPlayed)
+        {
+            GameObject[] trucks = GameObject.FindGameObjectsWithTag("Truck");
+            foreach (GameObject truck in trucks)
+            {
+                if (Vector3.Distance(truck.transform.position, player.position) <= activationDistance)
+                {
+                    PlaySound(sfx3);
+                    soundPlayed = true;
+                    break;
+                }
+            }
+        }
+
+
+
+        if (!soundPlayed && audiosystem.isPlaying)
+        {
+            audiosystem.Stop();
+        }
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (audiosystem.clip != clip || !audiosystem.isPlaying)
+        {
+            audiosystem.clip = clip;
+            audiosystem.Play();
+        }
     }
 
 }
